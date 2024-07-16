57b090e23f4749b4dbfa14c56aa1567a78445567
nithya222003 <nithyashreedurairajan@gmail.com>
2024-07-16 12:42:01 +0530

    initial commit

--- table.js
+++ table.js
@@ -1,12 +1,12 @@
-document.write("<table ><thead><th>flavours</th><th>cost</th></thead>"); 
+document.write("<table ><thead><th>cost</th><th>flavours</th></thead>"); 
 for(var i=0;i<1;i++) 
     {
          for(var j=0;j<1;j++) 
             {
-                 document.write("<tr><td>3</td><td>butter scotch</td></tr>");
-                 document.write("<tr><td>4</td><td>vanilla</td></tr>");
-                 document.write("<tr><td>1</td><td>choclate</td></tr>");
-                 document.write("<tr><td>8</td><td>pine apple</td></tr>");
+                 document.write("<tr><td>30</td><td>butter scotch</td></tr>");
+                 document.write("<tr><td>40</td><td>vanilla</td></tr>");
+                 document.write("<tr><td>10</td><td>choclate</td></tr>");
+                 document.write("<tr><td>80</td><td>pine apple</td></tr>");
                  };
                  };
                   document.write("</table>");
